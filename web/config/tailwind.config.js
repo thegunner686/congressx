11c995d547b51ffd2fec01e554a8c784660fea70
thegunner686 <gunn686@gmail.com>
2023-08-26 09:30:05 -0400

    purge config changes

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -3,6 +3,13 @@ const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,jsx,ts,tsx}",
+      "./src/components/**/*.{js,jsx,ts,tsx}",
+    ],
+    // These options are passed through directly to PurgeCSS
+  },
   theme: {
     extend: {
       textShadow: {
