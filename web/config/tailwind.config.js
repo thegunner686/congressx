c586f91a82ca4f7b5997737898fcb0fdcb9099e3
thegunner686 <gunn686@gmail.com>
2023-08-26 11:45:51 -0400

    new homepage

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -9,20 +11,29 @@ module.exports = {
         lg: "0 8px 16px var(--tw-shadow-color)",
       },
       colors: {
-        licorice: "#13070C",
-        gunmetal: "#2A2D34",
-        "fire-engine-red": "#C1292E",
-        "honolulu-blue": "#197BBD",
-        aquamarine: "#36F1CD",
-        dark: "#373D37",
-        "virginia-blue": "#35575F",
-        "virginia-brown": "#8C6915",
-        "virginia-orange": "#C7760F",
-        "va-green": "#818E5C",
-        "va-dark-green": "#464C24",
-        "house-orange": "#FC995A",
+        "crayola-red": "#EE4266",
+        night: "#080C0B",
+        silver: "#C4CBCA",
+        "majorelle-blue": "#724CF9",
+        verdigris: "#3CBBB1",
+      },
+      fontFamily: {
+        anton: ["Anton", "sans-serif"],
+        bebas: ["Bebas Neue", "sans-serif"],
+        archivo: ["Archivo Black", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") },
+      );
+    }),
+  ],
 };
